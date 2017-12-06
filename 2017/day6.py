58e42f8ad3bd7505ee02266fb41a1effee10feca
Hervé Martinet <hmartinet@users.noreply.github.com>
2017-12-06 19:01:59 +0100

    Update day6.py

--- 2017/day6.py
+++ 2017/day6.py
@@ -5,14 +5,19 @@ with open('day6.input') as f:
     mem = [int(n.strip()) for n in f.read().split('\t')]
 
 def execute(mem):
-    hist, m = [], mem.copy()
+    step = 0
+    m = mem[:]
+    hist = {str(m): step}
     while True:
-        hist.append(m.copy())
-        i, v = max(enumerate(m), key=lambda m: m[1])
-        m[i] = 0
-        for r in range(v):
-            m[(i + r + 1) % len(m)] += 1
-        if m in hist:
-            return len(hist), len(hist) - hist.index(m)
+        p = max(enumerate(m), key=lambda m: m[1])
+        l = len(m)
+        m[p[0]] = 0
+        m = [v + p[1] // l + (((i-p[0]-1) % l) < p[1] % l and 1) 
+             for i, v in tuple(enumerate(m))]
+        strm = str(m)
+        if strm in hist:
+            return len(hist), len(hist) - hist[strm]
+        step += 1
+        hist[strm] = step
 
 print("Solutions: [{}] [{}]".format(*execute(mem)))
