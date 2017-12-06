eba9e6959cd88756e009fc97e1db1818c2d56716
Hervé Martinet <hmartinet@users.noreply.github.com>
2017-12-06 07:09:10 +0100

    Add files via upload
    Day 6

---
+++ 2017/day6.py
@@ -0,0 +1,23 @@
+#!/ubr/bin/env python3
+#-*- coding: utf-8 -*-
+
+with open('day6.input') as f:
+    b = [int(n.strip()) for n in f.read().split('\t')]
+h = [b.copy()]
+
+def execute():
+    step = 0
+    while True:
+        step += 1
+        m = max(b)
+        i = b.index(m)
+        b[i] = 0
+        for r in range(m):
+            i = (i + 1) % len(b)
+            b[i] += 1
+
+        if b in h:
+            return step, len(h) - h.index(b)
+        h.append(b.copy())
+
+print("Solutions: [{}] [{}]".format(*execute()))
