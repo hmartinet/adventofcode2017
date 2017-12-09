3f69b350a075a473a4e2b1f6d9d68b93503623db
Hervé Martinet <hmartinet@users.noreply.github.com>
2017-12-09 08:53:47 +0100

    Update day9.py

--- 2017/day9.py
+++ 2017/day9.py
@@ -8,16 +8,17 @@ m = [0, 0, 0]
 res1, res2 = 0, 0
 default = lambda m: (m[0] and -1 or 0, 0, 0)
 parse = lambda m: {
-    '!': not m[0] and (1, 0, 0) or default(m),
-    '<': not sum(m[:2]) and (0, 1, 0) or default(m),
-    '>': not m[0] and m[1] and (0, -1, 0) or default(m),
-    '{': not sum(m[:2]) and (0, 0, 1) or default(m),
-    '}': not sum(m[:2]) and m[2] and (0, 0, -1) or default(m),
+    '!': not m[0] and (1, 0, 0),
+    '<': not sum(m[:2]) and (0, 1, 0),
+    '>': not m[0] and m[1] and (0, -1, 0),
+    '{': not sum(m[:2]) and (0, 0, 1),
+    '}': not sum(m[:2]) and m[2] and (0, 0, -1),
 }  
 for c in s:
-    p = parse(m).get(c, default(m))
+    p = parse(m).get(c) or default(m)
     res1 += p[2] == -1 and m[2]
     res2 += m[1] and not p[1] and not p[0] and 1
     m = [mi + p[i] for i, mi in enumerate(m)]
 
 print("Solutions: [{}] [{}]".format(res1, res2))
+
