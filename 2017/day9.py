bd5afc24991a69a54f71b8a52e2716f1c3e371bf
Hervé Martinet <hmartinet@users.noreply.github.com>
2017-12-09 09:17:13 +0100

    Update day9.py

--- 2017/day9.py
+++ 2017/day9.py
@@ -6,7 +6,7 @@ with open('day9.input') as f:
 
 m = 0, 0, 0
 res = 0, 0
-parse = lambda m: {
+parser = lambda m: {
     '!': not m[0] and (1, 0, 0),
     '<': not sum(m[:2]) and (0, 1, 0),
     '>': not m[0] and m[1] and (0, -1, 0),
@@ -14,7 +14,7 @@ parse = lambda m: {
     '}': not sum(m[:2]) and m[2] and (0, 0, -1),
 }  
 for c in s:
-    p = parse(m).get(c) or (m[0] and -1 or 0, 0, 0)
+    p = parser(m).get(c) or (m[0] and -1 or 0, 0, 0)
     res = (res[0] + (p[2] == -1 and m[2]),
            res[1] + (m[1] and not p[1] and not p[0] and 1))
     m = tuple(m[i] + p[i] for i in range(3))
