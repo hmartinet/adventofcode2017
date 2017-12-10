791fb98a67ef833887b96d916fc033ed05a1d6f6
Hervé Martinet <hmartinet@users.noreply.github.com>
2017-12-10 10:28:37 +0100

    Update day10.py

--- 2017/day10.py
+++ 2017/day10.py
@@ -11,9 +11,9 @@ def hash(data, rnd=64, ext=[17, 31, 73, 47, 23]):
     cr, skip = 0, 0
     for _ in range(rnd):
         for l in lengths:
-            sl = [h[(cr+i)%256] for i in range(l)][::-1]
+            sub = [h[(cr+i)%256] for i in range(l)][::-1]
             for i in range(l):
-                h[(cr+i)%256] = sl[i]
+                h[(cr+i)%256] = sub[i]
             cr, skip = (cr+l+skip)%256, skip+1
     return h
 
