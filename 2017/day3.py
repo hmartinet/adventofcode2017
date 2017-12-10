e472f1505daf189a2ef43aa811e00ad2736ef4fb
Hervé Martinet <hmartinet@users.noreply.github.com>
2017-12-10 11:54:17 +0100

    Update day3.py

--- 2017/day3.py
+++ 2017/day3.py
@@ -3,13 +3,15 @@
 
 input = 361527
 
+d = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+
+nsum = lambda x, y: sum([
+    s.get((x + dx, y + dy), 0)
+    for dx in (0, 1, -1)
+    for dy in (0, 1, -1)])
+
 def spiral(limit):
-    d = [(1, 0), (0, 1), (-1, 0), (0, -1)]
     s = {(0, 0): 1}
-    nsum = lambda x, y: sum([
-        s.get((x + dx, y + dy), 0)
-        for dx in (0, 1, -1)
-        for dy in (0, 1, -1)])
     n, x, y, i = 1, 0, 0, 0
     r1, r2 = 1, 0
     while True:
@@ -24,5 +26,3 @@ def spiral(limit):
             if r1 and r2:
                 return r1, r2
         i += 1
-
-print("Solutions: [{}] [{}]".format(*spiral(input)))
