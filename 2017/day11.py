fc78ca02d1bb2e8d5e93b12bc3d96affd18bf553
Hervé Martinet <hmartinet@users.noreply.github.com>
2017-12-11 07:18:45 +0100

    Update day11.py

--- 2017/day11.py
+++ 2017/day11.py
@@ -8,7 +8,7 @@ with open('day11.input') as f:
     path = f.read().strip().split(',')
 
 def distance(c):
-    x, y = abs(c[0]), abs(c[1])
+    x, y = tuple(abs(c[i]) for i in range(2))
     return y + max(0, (x - y) // 2)
 
 md = 0
