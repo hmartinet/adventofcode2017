f4816408357041c4e3441b2a7631b1613c26c017
Hervé Martinet <hmartinet@users.noreply.github.com>
2017-12-11 07:03:00 +0100

    Update day11.py

--- 2017/day11.py
+++ 2017/day11.py
@@ -1,18 +1,15 @@
 #!/usr/bin/env python3
 #-*- coding: utf-8 -*-
 
-import numpy
-
 coord = {'n': (2, 0), 'ne': (1, -1), 'se': (-1, -1), 
          's': (-2, 0), 'sw': (-1, 1), 'nw': (1, 1)}
 
 with open('day11.input') as f:
     path = f.read().strip().split(',')
 
-def distance(pos):
-    pos = numpy.abs(pos)
-    x, y = pos[...,0], pos[...,1]
-    return y + numpy.maximum(0, (x - y) // 2)
+def distance(c):
+    x, y = abs(c[0]), abs(c[1])
+    return y + max(0, (x - y) // 2)
 
 md = 0
 c = 0, 0
