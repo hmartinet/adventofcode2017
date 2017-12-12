8ce41ccbe9dbd27b70c190426e05eb8c33ec328a
Hervé Martinet <hmartinet@users.noreply.github.com>
2017-12-12 06:43:52 +0100

    Update day12.py

--- 2017/day12.py
+++ 2017/day12.py
@@ -12,15 +12,15 @@ with open('day12.input') as f:
         s[int(m.group(1))] = [
             int(e) for e in m.group(2).split(', ')]
 
-def walk(s, prog, count, bp):
+def walk(s, prog, bp):
     bp.add(prog)
     for p in set(s[prog]) - bp:
-        bp.update(walk(s, p, count, bp))
+        bp.update(walk(s, p, bp))
     return bp
 
 groups = []
 for p in s.keys():
     if p not in [e for g in groups for e in g]:
-        groups.append(walk(s, p, 0, set()))
+        groups.append(walk(s, p, set()))
 
 print("Solutions: [{}] [{}]".format(len(groups[0]), len(groups)))
