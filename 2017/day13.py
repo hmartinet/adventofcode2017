7fdbef4e09dbb5e833addba7ee8c3692b1899650
Hervé Martinet <hmartinet@users.noreply.github.com>
2017-12-13 09:09:26 +0100

    Update day13.py

--- 2017/day13.py
+++ 2017/day13.py
@@ -6,8 +6,8 @@ with open('day13.input') as f:
 
 run = lambda s, d: sum((i in s and (i+d) % ((s[i]-1)*2) == 0) and
                        ((i+d) * s[i]) for i in range(max(s.keys()) + 1))
-r1, r2, c = run(s, 0), -1, 1
+r1, r2, c = run(s, 0), 0, 1
 while c != 0:
-    r2, c = r2 + 1, run(s, r2 + 1)
+    r2, c = r2 + 1, run(s, r2)
 
-print("Solutions: [{}] [{}]".format(r1, r2))
+print("Solutions: [{}] [{}]".format(r1, r2-1))
