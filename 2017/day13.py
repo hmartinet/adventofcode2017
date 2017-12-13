679c77d039563c38f627f9d7dee8aa9b03693ffc
Hervé Martinet <hmartinet@users.noreply.github.com>
2017-12-13 09:29:12 +0100

    Update day13.py

--- 2017/day13.py
+++ 2017/day13.py
@@ -2,10 +2,9 @@
 #-*- coding: utf-8 -*-
 
 with open('day13.input') as f:
-    s = dict((int(n) for n in l.strip().split(': ')) for l in f.readlines())
+    s = [tuple(int(n) for n in l.strip().split(': ')) for l in f.readlines()]
 
-run = lambda s, d: sum((i in s and (i+d) % ((s[i]-1)*2) == 0) and
-                       ((i+d) * s[i]) for i in range(max(s.keys()) + 1))
+run = lambda s, d: sum((i+d) % (2*(r-1)) == 0 and (i+d) * r for i, r in s)
 r1, r2, c = run(s, 0), 0, 1
 while c != 0:
     r2, c = r2 + 1, run(s, r2)
