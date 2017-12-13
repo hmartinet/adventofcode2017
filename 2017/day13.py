f24141ddcb28c4d5fe51a7bafa04c1477d69c36d
Hervé Martinet <hmartinet@users.noreply.github.com>
2017-12-13 08:58:58 +0100

    Update day13.py

--- 2017/day13.py
+++ 2017/day13.py
@@ -1,24 +1,15 @@
 #!/usr/bin/env python3
 #-*- coding: utf-8 -*-
 
-import re
-
-parse = re.compile(r"^([0-9]+): ([0-9]+)$")
-
-s = {}
 with open('day13.input') as f:
-    for l in f.readlines():
-        m = parse.match(l)
-        s[int(m.group(1))] = int(m.group(2))
+    s = dict((int(n) for n in l.strip().split(': ')) for l in f.readlines())
 
 def run(s, d):
     return sum((i in s and (i + d) % ((s[i] - 1) * 2) == 0) and
                ((i + d) * s[i]) for i in range(max(s.keys()) + 1))
 
-r = [run(s, 0), 0]
-while True:
-    if run(s, r[1]) == 0:
-        break
-    r[1] += 1
+r = [run(s, 0), (-1, -1)]
+while r[1][1] != 0:
+    r[1] = r[1][0] + 1, run(s, r[1][0] + 1)
 
 print("Solutions: [{}] [{}]".format(*r))
