55c27d1d800c9d3fc88c232bd4b9ce3b7ed0c8a7
Hervé Martinet <hmartinet@users.noreply.github.com>
2017-12-13 09:05:12 +0100

    Update day13.py

--- 2017/day13.py
+++ 2017/day13.py
@@ -4,12 +4,10 @@
 with open('day13.input') as f:
     s = dict((int(n) for n in l.strip().split(': ')) for l in f.readlines())
 
-def run(s, d):
-    return sum((i in s and (i + d) % ((s[i] - 1) * 2) == 0) and
-               ((i + d) * s[i]) for i in range(max(s.keys()) + 1))
+run = lambda s, d: sum((i in s and (i + d) % ((s[i] - 1) * 2) == 0) and
+                       ((i + d) * s[i]) for i in range(max(s.keys()) + 1))
+r1, r2, c = run(s, 0), -1, 1
+while c != 0:
+    r2, c = r2 + 1, run(s, r2 + 1)
 
-r = [run(s, 0), (-1, -1)]
-while r[1][1] != 0:
-    r[1] = r[1][0] + 1, run(s, r[1][0] + 1)
-
-print("Solutions: [{}] [{}]".format(*r))
+print("Solutions: [{}] [{}]".format(r1, r2))
