b1af37fc67e46f3cbdc062c83f06238e65e23a4d
Hervé Martinet <herve.martinet@gmail.com>
2017-12-15 09:52:45 +0100

    Day 15

---
+++ aoc2017/day15.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from datetime import datetime as dt
+
+t = dt.now()
+
+class Generator():
+    
+    def __init__(self, *args):
+        self.f1, self.f2, self.val, self.mod = args
+
+    def next(self):
+        self.val = (self.val * self.f1) % self.f2
+        if self.val % self.mod != 0:
+            self.next()
+
+    def bin16(self):
+        return '{:032b}'.format(self.val)[16:]
+
+
+def resolve(g1, g2, iters):
+    i, count = 0, 0
+    while i < iters:
+        g1.next()
+        g2.next()
+        count += g1.bin16() == g2.bin16()
+        i += 1
+    return count
+
+r1 = resolve(Generator(16807, 2147483647, 116, 1),
+             Generator(48271, 2147483647, 299, 1),
+             40000000)
+r2 = resolve(Generator(16807, 2147483647, 116, 4),
+             Generator(48271, 2147483647, 299, 8),
+             5000000)
+
+print("Solutions: [{}] [{}]".format(r1, r2))
