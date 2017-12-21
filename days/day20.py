89d6cd355ab17111cf1d1e75b7b6671e72383ec8
Hervé Martinet <herve.martinet@gmail.com>
2017-12-21 06:31:43 +0100

    Day 20

--- days/day20.py
+++ days/day20.py
@@ -26,6 +26,25 @@ def tick(parts):
     return c
 
 
+def add(v1, v2):
+    return V(v1.x + v2.x, v1.y + v2.y, v1.z + v2.z)
+
+
+def sub(v1, v2):
+    return V(v1.x - v2.x, v1.y - v2.y, v1.z - v2.z)
+
+
+def divide(v1, v2):
+    if v1.x / v2.x == v1.y / v2.y == v1.z / v2.z:
+        return v1.x / v2.x
+    else:
+        raise
+
+
+def collide(p1, p2):
+    return divide(sub(p2.p, p1.p), add(p1.v, p2.v))
+
+
 def solve(din):
     parts = []
     for l in din.readlines():
@@ -35,15 +54,21 @@ def solve(din):
             V(*[int(v) for v in g[3:6]]),
             V(*[int(v) for v in g[6:9]])))
 
-    c = None
-    for i, p in enumerate(parts):
-        print((p, abs(p.p.x) + abs(p.p.y) + abs(p.p.z)))
-        if not c or abs(p.p.x) + abs(p.p.y) + abs(p.p.z) < c[1]:
-            c = i, abs(p.p.x) + abs(p.p.y) + abs(p.p.z)
-    for _ in range(4):
-        print('-')
-        cl = tick(parts)
-        if not c or cl[1] < c[1]:
-            c = cl
+    collisions = {}
+    for i, p1 in enumerate(parts):
+        for p2 in parts[i + 1:]:
+            try:
+                n = collide(p1, p2)
+                collisions[n] = collisions.get(n, []) + [(p1, p2)]
+            except Exception:
+                pass
 
-    return c, 1
+    sparts = parts[:]
+    for n in sorted(collisions.keys()):
+        for pair in collisions[n]:
+            if pair[0] in sparts and pair[1] in sparts:
+                print(pair)
+                sparts.remove(pair[0])
+                sparts.remove(pair[1])
+
+    return 1, len(sparts)
