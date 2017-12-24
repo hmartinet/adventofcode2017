d9122fcae26adc02171a72ed7bded3fca170e80b
Hervé Martinet <herve.martinet@gmail.com>
2017-12-24 09:19:55 +0100

    Enable all tests

--- test_days.py
+++ test_days.py
@@ -8,8 +8,8 @@ logging.basicConfig(level=logging.DEBUG)
 log = logging.getLogger('tests')
 
 
-def dtest_day01():
-    log.info("dtest_day01")
+def test_day01():
+    log.info("test_day01")
     d = days.day01
     assert d.solve(sio("1122"))[0] == 3
     assert d.solve(sio("1111"))[0] == 4
@@ -24,8 +24,8 @@ def dtest_day01():
         assert d.solve(f) == (1175, 1166)
 
 
-def dtest_day02():
-    log.info("dtest_day02")
+def test_day02():
+    log.info("test_day02")
     d = days.day02
     with open('input/day02.test1') as f:
         assert d.solve(f)[0] == 18
@@ -35,8 +35,8 @@ def dtest_day02():
         assert d.solve(f) == (45972, 326)
 
 
-def dtest_day03():
-    log.info("dtest_day03")
+def test_day03():
+    log.info("test_day03")
     d = days.day03
     assert d.solve(sio("1"))[0] == 0
     assert d.solve(sio("12"))[0] == 3
@@ -46,8 +46,8 @@ def dtest_day03():
         assert d.solve(f) == (326, 363010)
 
 
-def dtest_day04():
-    log.info("dtest_day04")
+def test_day04():
+    log.info("test_day04")
     d = days.day04
     assert d.solve(sio("aa bb cc dd ee"))[0] == 1
     assert d.solve(sio("aa bb cc dd aa"))[0] == 0
@@ -61,8 +61,8 @@ def dtest_day04():
         assert d.solve(f) == (386, 208)
 
 
-def dtest_day05():
-    log.info("dtest_day05")
+def test_day05():
+    log.info("test_day05")
     d = days.day05
     with open('input/day05.test') as f:
         assert d.solve(f) == (5, 10)
@@ -70,16 +70,16 @@ def dtest_day05():
         assert d.solve(f) == (378980, 26889114)
 
 
-def dtest_day06():
-    log.info("dtest_day06")
+def test_day06():
+    log.info("test_day06")
     d = days.day06
     assert d.solve(sio("0\t2\t7\t0")) == (5, 4)
     with open('input/day06') as f:
         assert d.solve(f) == (7864, 1695)
 
 
-def dtest_day07():
-    log.info("dtest_day07")
+def test_day07():
+    log.info("test_day07")
     d = days.day07
     with open('input/day07.test') as f:
         assert d.solve(f) == ('tknk', 60)
@@ -87,8 +87,8 @@ def dtest_day07():
         assert d.solve(f) == ('eqgvf', 757)
 
 
-def dtest_day08():
-    log.info("dtest_day08")
+def test_day08():
+    log.info("test_day08")
     d = days.day08
     with open('input/day08.test') as f:
         assert d.solve(f) == (1, 10)
@@ -96,8 +96,8 @@ def dtest_day08():
         assert d.solve(f) == (4163, 5347)
 
 
-def dtest_day09():
-    log.info("dtest_day09")
+def test_day09():
+    log.info("test_day09")
     d = days.day09
     assert d.solve(sio("{}"))[0] == 1
     assert d.solve(sio("{{{}}}"))[0] == 6
@@ -118,8 +118,8 @@ def dtest_day09():
         assert d.solve(f) == (11846, 6285)
 
 
-def dtest_day10():
-    log.info("dtest_day10")
+def test_day10():
+    log.info("test_day10")
     from common import knot
     h = knot.knot([int(n) for n in "3, 4, 1, 5".split(',')], 1, [], 5)
     assert h[0] * h[1] == 12
@@ -132,8 +132,8 @@ def dtest_day10():
         assert d.solve(f) == (4480, 'c500ffe015c83b60fad2e4b7d59dabc4')
 
 
-def dtest_day11():
-    log.info("dtest_day11")
+def test_day11():
+    log.info("test_day11")
     d = days.day11
     assert d.solve(sio("ne,ne,ne"))[0] == 3
     assert d.solve(sio("ne,ne,sw,sw"))[0] == 0
@@ -143,8 +143,8 @@ def dtest_day11():
         assert d.solve(f) == (696, 1461)
 
 
-def dtest_day12():
-    log.info("dtest_day12")
+def test_day12():
+    log.info("test_day12")
     d = days.day12
     with open('input/day12.test') as f:
         assert d.solve(f) == (6, 2)
@@ -152,8 +152,8 @@ def dtest_day12():
         assert d.solve(f) == (169, 179)
 
 
-def dtest_day13():
-    log.info("dtest_day13")
+def test_day13():
+    log.info("test_day13")
     d = days.day13
     with open('input/day13.test') as f:
         assert d.solve(f) == (24, 10)
@@ -161,8 +161,8 @@ def dtest_day13():
         assert d.solve(f) == (1900, 3966414)
 
 
-def dtest_day14():
-    log.info("dtest_day14")
+def test_day14():
+    log.info("test_day14")
     d = days.day14
     with open('input/day14.test') as f:
         assert d.solve(f) == (8108, 1242)
@@ -170,8 +170,8 @@ def dtest_day14():
         assert d.solve(f) == (8316, 1074)
 
 
-def dtest_day15():
-    log.info("dtest_day15")
+def test_day15():
+    log.info("test_day15")
     d = days.day15
     with open('input/day15.test') as f:
         assert d.solve(f) == (588, 309)
@@ -179,8 +179,8 @@ def dtest_day15():
         assert d.solve(f) == (569, 298)
 
 
-def dtest_day16():
-    log.info("dtest_day16")
+def test_day16():
+    log.info("test_day16")
     d = days.day16
     with open('input/day16.test') as f:
         ops = d.parse_input(f)
@@ -190,17 +190,17 @@ def dtest_day16():
         assert d.solve(f) == ('kpbodeajhlicngmf', 'ahgpjdkcbfmneloi')
 
 
-def dtest_day17():
-    log.info("dtest_day17")
+def test_day17():
+    log.info("test_day17")
     d = days.day17
     assert d.solve(sio("3"))[0] == 638
     with open('input/day17') as f:
         assert d.solve(f) == (1282, 27650600)
 
 
-def dtest_day18():
+def test_day18():
     # TODO: There is a problem with multiprocessing on Travis CI
-    # log.info("dtest_day18")
+    # log.info("test_day18")
     # d = days.day18
     # with open('input/day18.test1') as f:
     #     assert d.solve(f)[0] == 4
@@ -211,8 +211,8 @@ def dtest_day18():
     pass
 
 
-def dtest_day19():
-    log.info("dtest_day19")
+def test_day19():
+    log.info("test_day19")
     d = days.day19
     with open('input/day19.test') as f:
         assert d.solve(f) == ('ABCDEF', 38)
@@ -220,8 +220,8 @@ def dtest_day19():
         assert d.solve(f) == ('RUEDAHWKSM', 17264)
 
 
-def dtest_day20():
-    log.info("dtest_day20")
+def test_day20():
+    log.info("test_day20")
     d = days.day20
     with open('input/day20.test1') as f:
         assert d.solve(f)[0] == 0
@@ -231,8 +231,8 @@ def dtest_day20():
         assert d.solve(f) == (243, 648)
 
 
-def dtest_day21():
-    log.info("dtest_day21")
+def test_day21():
+    log.info("test_day21")
     d = days.day21
     with open('input/day21.test') as f:
         assert d.solve_for(f, 2)[0] == 12
@@ -240,8 +240,8 @@ def dtest_day21():
         assert d.solve(f) == (167, 2425195)
 
 
-def dtest_day22():
-    log.info("dtest_day22")
+def test_day22():
+    log.info("test_day22")
     d = days.day22
     with open('input/day22.test') as f:
         assert d.solve_for(d.parse(f), 7, 0) == 5
@@ -257,15 +257,15 @@ def dtest_day22():
         assert d.solve(f) == (5176, 2512017)
 
 
-def dtest_day23():
-    log.info("dtest_day23")
+def test_day23():
+    log.info("test_day23")
     d = days.day23
     with open('input/day23') as f:
         assert d.solve(f) == (6724, 903)
 
 
 def test_day24():
-    log.info("dtest_day24")
+    log.info("test_day24")
     d = days.day24
     with open('input/day24.test') as f:
         assert d.solve(f) == (31, 19)
